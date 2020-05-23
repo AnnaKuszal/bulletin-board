d479e6a3c4cc742a4ecc2f60fdd34223985a0d2c
Anna Kuszal <anna.kuszal@o2.pl>
2020-05-24 01:18:01 +0200

    Add Homepage component

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -2,27 +2,62 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
-
 import styles from './Homepage.module.scss';
 
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
-const Component = ({className, children}) => (
+//import { settings } from '../../../settings';
+
+import { connect } from 'react-redux';
+// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { getAll } from '../../../redux/postsRedux.js';
+
+
+const Component = ({ className, posts }) => (
   <div className={clsx(className, styles.root)}>
     <h2>Homepage</h2>
-    {children}
+
+    <Container maxWidth="lg">
+      <Button color="secondary" aria-label="add" className={styles.button}>
+        <AddIcon /> Add new
+      </Button>
+
+      {posts.map(el => (
+        <Card key={el.id} className={styles.card}>
+          <CardContent>
+            <Typography gutterBottom >
+              <Link href="#" className={styles.title}>
+                {el.title}
+              </Link>
+            </Typography>
+            <Typography className={styles.subtitle} gutterBottom>
+              Date: {el.date} / Modified: {el.updateDate}
+            </Typography>
+          </CardContent>
+
+        </Card>
+      ))}
+
+    </Container>
+
   </div>
 );
 
 Component.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  posts: PropTypes.array,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  posts: getAll(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
@@ -30,9 +65,11 @@ Component.propTypes = {
 
 // const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
+const HomepageContainer = connect(mapStateToProps)(Component);
 
 export {
-  Component as Homepage,
-  // Container as Homepage,
+  //Component as Homepage,
+  //Container as Homepage,
+  HomepageContainer as Homepage,
   Component as HomepageComponent,
 };
--- src/components/views/Homepage/Homepage.test.js
+++ src/components/views/Homepage/Homepage.test.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { HomepageComponent } from './Homepage';
 
+
+const mockProps = {
+  posts: [
+    { id: '1', title: 'title 1', content: 'content 1' },
+    { id: '2', title :'title 2', content: 'content 2' },
+  ],
+};
+
+
 describe('Component Homepage', () => {
   it('should render without crashing', () => {
-    const component = shallow(<HomepageComponent />);
+    const component = shallow(<HomepageComponent {...mockProps} />);
     expect(component).toBeTruthy();
   });
 });
