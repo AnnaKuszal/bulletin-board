37fd18ca61bd104ee5881ef1b5c61a78b16475f0
Anna Kuszal <anna.kuszal@o2.pl>
2020-05-23 17:54:06 +0200

    Add MainLayout (component&test)

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -7,9 +7,11 @@ import clsx from 'clsx';
 
 import styles from './MainLayout.module.scss';
 
+import { Header } from '../Header/Header';
+
 const Component = ({className, children}) => (
   <div className={clsx(className, styles.root)}>
-    <h2>MainLayout</h2>
+    <Header />
     {children}
   </div>
 );
