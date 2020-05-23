5001dedcaadab01793a5138d33ea9cf40b591a32
Anna Kuszal <anna.kuszal@o2.pl>
2020-05-24 01:40:56 +0200

    Add NotFound view

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -2,21 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
+import styles from './NotFound.module.scss';
+import { settings } from '../../../settings';
+
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
-import styles from './NotFound.module.scss';
-
-
-const Component = ({className, children}) => (
+const Component = ({className}) => (
   <div className={clsx(className, styles.root)}>
-    <h2>NotFound</h2>
-    {children}
+
+    <h2>Sorry, the page you`&apos;`re looking for was not found :(</h2>
+    <Container maxWidth="lg">
+      <img src={settings.notFound} alt="404 error" className={styles.image} />
+      <Button href='/' className={styles.btn}>Go to homepage</Button>
+    </Container>
+
   </div>
 );
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
 };
 
