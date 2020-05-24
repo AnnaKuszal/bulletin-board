3057fc05b2dfaa7dcced0c7452117c753efd7ae5
Anna Kuszal <anna.kuszal@o2.pl>
2020-05-25 01:38:47 +0200

    Add 'PostAdd' view

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -2,21 +2,96 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
+import styles from './PostAdd.module.scss';
+
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
-import styles from './PostAdd.module.scss';
 
+const Component = ({className}) => {
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>PostAdd</h2>
-    {children}
-  </div>
-);
+  const [value, setValue] = React.useState('');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('Submitted');
+  };
+
+  return (
+
+    <div className={clsx(className, styles.root)}>
+      <h2>Add new post</h2>
+
+      <Container maxWidth="lg">
+
+        <Card className={styles.card}>
+
+          <Typography gutterBottom className={styles.title}>
+            Edit post
+          </Typography>
+
+          <form className={styles.form} onSubmit={e => handleSubmit(e)}>
+            <TextField
+              id="title"
+              label="Title"
+              required
+              className={styles.textInput}
+              onChange={handleChange}
+            />
+
+            <TextField
+              variant="outlined"
+              multiline
+              id="content"
+              label="Content"
+              placeholder="Text"
+              rows="6"
+              required
+              className={styles.textInput}
+              onChange={handleChange}
+
+            />
+            <TextField
+              id="mail"
+              label="E-mail"
+              type="email"
+              required
+              className={styles.textInput}
+              onChange={handleChange}
+            />
+            <TextField
+              id="phone"
+              label="Phone number"
+              type="string"
+              className={styles.textInput}
+              onChange={handleChange}
+            />
+
+            <div>
+              <Button type="submit" className={styles.btn}>Save</Button>
+              <Button href="/" className={styles.btn}>Return</Button>
+            </div>
+          </form>
+        </Card>
+
+      </Container>
+
+    </div>
+
+  );
+};
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
 };
 
