b91f870861cb7d71ac67532c1c3d70c5c0c2c55a
Anna Kuszal <anna.kuszal@o2.pl>
2020-05-26 01:48:49 +0200

    Add settings.js

---
+++ src/settings.js
@@ -0,0 +1,8 @@
+export const api = {
+  url: '//' + window.location.hostname + (window.location.hostname === 'localhost' ? ':3000/api' : ''),
+  posts: 'posts',
+};
+
+export const settings = {
+  notFound: 'https://3suxxy8wyhp1vv6ln2ofn5a148f-wpengine.netdna-ssl.com/wp-content/uploads/jpg/bg-img-404-error-1920x1080.jpg',
+};
